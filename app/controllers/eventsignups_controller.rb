ac8e3598b9bcc2605fbd77379ec767a3c3238ee0
Matthias Meier <meierm@uchicago.edu>
2015-05-30 13:12:09 -0500

    Add comments

--- app/controllers/eventsignups_controller.rb
+++ app/controllers/eventsignups_controller.rb
@@ -3,7 +3,7 @@ class EventsignupsController < ApplicationController
     before_action :require_user, :only => [:create, :destroy]
     before_action :authorize, :only => [:destroy] # only allow a user to delete his own event signups
     
-    # Only allow the user who created the event to delete an event signup
+    # Only allow a user to delete own event signups
     def authorize
         @user_ids = EventSignup.where(event_id: params["event_id"]).pluck(:user_id)
         if @user_ids.blank? || !@user_ids.include?(session[:user_id])
@@ -11,6 +11,7 @@ class EventsignupsController < ApplicationController
         end
     end 
 
+    #make sure the user is logged in to sign up for an event
     def require_user
       if session[:user_id].blank?
         redirect_to events_url, notice: "You need to login to do that."
