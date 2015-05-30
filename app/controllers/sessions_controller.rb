b3b45942117b1fbd88e838986295eda191b50912
Matthias Meier <meierm@uchicago.edu>
2015-05-29 21:44:40 -0500

    Update session ids

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,7 +7,8 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by(email: params[:email])
     if user
-      if user.authenticate(params[:password])
+      if user.authenticate(params[:password]) 
+        session["user_id"] = user.id
         cookies["user_id"] = user.id
         redirect_to root_url, notice: "Welcome!"
       else
@@ -20,6 +21,7 @@ class SessionsController < ApplicationController
 
   def destroy
     cookies["user_id"] = nil
+    session["user_id"] = nil
     redirect_to root_url, notice: "Logged out."
   end
 
