63a6a24df60fb09ce67f8dce85212be4caeea369
Matthias Meier <meierm@uchicago.edu>
2015-05-31 11:19:43 -0500

    Add Password resets

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,28 +1,28 @@
 class SessionsController < ApplicationController
-
-  def new
-
-  end
-
-  def create
-    user = User.find_by(email: params[:email])
-    if user
-      if user.authenticate(params[:password]) 
-        session["user_id"] = user.id
-        cookies["user_id"] = user.id
-        redirect_to root_url, notice: "Welcome!"
-      else
-        redirect_to root_url, notice: "Unknown password."
-      end
-    else
-      redirect_to root_url, notice: "Unknown email."
+    
+    def new
+        
     end
-  end
-
-  def destroy
-    cookies["user_id"] = nil
-    session["user_id"] = nil
-    redirect_to root_url, notice: "Logged out."
-  end
-
+    
+    def create
+        user = User.find_by(email: params[:email])
+        if user
+            if user.authenticate(params[:password])
+                session["user_id"] = user.id
+                cookies["user_id"] = user.id
+                redirect_to root_url, notice: "Welcome!"
+                else
+                redirect_to root_url, notice: "Unknown password."
+            end
+            else
+            redirect_to root_url, notice: "Unknown email."
+        end
+    end
+    
+    def destroy
+        cookies["user_id"] = nil
+        session["user_id"] = nil
+        redirect_to root_url, notice: "Logged out."
+    end
+    
 end
