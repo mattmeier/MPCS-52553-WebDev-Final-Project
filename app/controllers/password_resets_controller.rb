eab742e97114f9e8fe6c4439a95a150c1f7ff1bd
Matthias Meier <meierm@uchicago.edu>
2015-05-31 12:38:13 -0500

    Change reset text

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -8,7 +8,7 @@ class PasswordResetsController < ApplicationController
         user = User.find_by_email(params[:email])
         
         user.send_password_reset if user
-        redirect_to root_url, :notice => "If the inputted email belongs to a user, an email has been sent with password reset instructions."
+        redirect_to root_url, :notice => "An email has been sent with password reset instructions."
         
     end
     
